caa6fef31133cfca5e1e2e509b74e6e2a3b1d7da
EdoCrippaOfficial <edoardocrippa96@gmail.com>
2020-04-03 23:16:49 +0200

    Aggiunta classe Messaggio, gestione (con sys.err, poi metteremo una eccezione carina) del CC con singolo destinatario

--- SmartMessaging/src/smartmessageCustomListener.java
+++ SmartMessaging/src/smartmessageCustomListener.java
@@ -1,39 +1,60 @@
-import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.List;
 
 public class smartmessageCustomListener extends smartmessageBaseListener {
 
-    private HashMap<String, String> variabili = new HashMap<>();
+    private int priorita = 0;
+    private List<Messaggio> messaggi = new ArrayList<>();
+    private Messaggio current_msg;
+
 
     @Override
     public void exitPriorita(smartmessageParser.PrioritaContext ctx) {
-        variabili.put("priorita", ctx.NUM().getText());
+        priorita = Integer.parseInt(ctx.NUM().getText());
         System.out.println("PRIORITA' " + ctx.NUM().getText());
     }
 
     @Override
     public void exitInvia(smartmessageParser.InviaContext ctx) {
         System.out.println("INVIA");
+        // Effettivamente inviamo i messaggi salvati
+        messaggi.clear();
+        current_msg = null;
+    }
+
+    @Override
+    public void enterMessage(smartmessageParser.MessageContext ctx) {
+        Messaggio new_msg = new Messaggio(priorita);
+        messaggi.add(new_msg);
+        current_msg = new_msg;
+        System.out.println("CREATO NUOVO MESSAGGIO");
     }
 
     @Override
     public void exitTitolo(smartmessageParser.TitoloContext ctx) {
-        variabili.put("titolo", ctx.TESTO().getText());
-        System.out.println("TITOLO " + ctx.TESTO().getText());
+        String testo = removeQuotes(ctx.TESTO().getText());
+        current_msg.titolo = testo;
+        System.out.println("TITOLO " + testo);
     }
 
     @Override
     public void exitCorpo(smartmessageParser.CorpoContext ctx) {
-        variabili.put("corpo", ctx.TESTO().getText());
-        System.out.println("CORPO " + ctx.TESTO().getText());
+        String testo = removeQuotes(ctx.TESTO().getText());
+        current_msg.corpo = testo;
+        System.out.println("CORPO " + testo);
     }
 
     @Override
     public void exitOpzioni(smartmessageParser.OpzioniContext ctx) {
         if (ctx.cc() != null){
-            variabili.put("cc", "true");
+            if (current_msg.destinatari.size() == 1)
+                System.err.println("Error at line " + ctx.start.getLine() +
+                        " position " + ctx.start.getCharPositionInLine() +
+                        ". Non è possibile usare CC se c'è un singolo destinatario");
+            current_msg.cc = true;
             System.out.println("CC TRUE");
         } else {
-            variabili.put("cc", "false");
+            current_msg.cc = false;
             System.out.println("CC FALSE");
         }
     }
@@ -41,24 +62,33 @@ public class smartmessageCustomListener extends smartmessageBaseListener {
     @Override
     public void exitImg(smartmessageParser.ImgContext ctx) {
         if (ctx.TESTO() != null){
-            variabili.put("img", ctx.TESTO().getText());
-            System.out.println("IMG " + ctx.TESTO().getText());
+            String testo = removeQuotes(ctx.TESTO().getText());
+            current_msg.img = testo;
+            System.out.println("IMG " + testo);
         }
     }
 
     @Override
     public void exitDestinatario(smartmessageParser.DestinatarioContext ctx) {
         if (ctx.TESTO() != null) {
-            variabili.put("dest", ctx.TESTO().getText());
-            System.out.println("DESTINATARIO " + ctx.TESTO().getText());
+            String testo = removeQuotes(ctx.TESTO().getText());
+            current_msg.destinatari.add(testo);
+            System.out.println("DESTINATARIO " + testo);
         }
     }
 
     @Override
     public void exitFormat(smartmessageParser.FormatContext ctx) {
         if (ctx.TESTO() != null) {
-            variabili.put("format", ctx.TESTO().getText());
-            System.out.println("FORMATTAZIONE " + ctx.TESTO().getText());
+            String testo = removeQuotes(ctx.TESTO().getText());
+            if (testo.equals("All caps")){
+                current_msg.formattazione = Fromat.ALL_CAPS;
+                System.out.println("FORMATTAZIONE " + testo);
+            }
         }
+    }
+
+    private String removeQuotes(String input){
+        return input.substring(1, input.length() - 1);
     }
 }
